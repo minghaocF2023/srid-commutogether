0ef320e74c392bc09e0d3d2e4254791bea676e95
Leo Chen <minghaoc@andrew.cmu.edu>
2024-12-03 15:38:24 -0800

    finish update location

---
+++ src/app/stamps/view/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import useLocalStorage from "@/hook/useLocalStorage";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import StyledLink from "@/components/StyledLink";
+type LocationData = {
+  name: string;
+  stampImage: string;
+  image: string;
+  intro: string;
+  collected: boolean;
+};
+const View = () => {
+  const searchParams = useSearchParams();
+  const [stampData] = useLocalStorage<{ [key: string]: LocationData }>(
+    "stampData",
+    {}
+  );
+  const [location, setLocation] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (searchParams.has("location")) {
+      setLocation(searchParams.get("location"));
+    }
+  }, [searchParams]);
+
+  return (
+    <>
+      {location ? (
+        <div className="p-4 flex flex-col items-center justify-center gap-10">
+          <h1 className="text-3xl text-center mt-10 text-slate-500">
+            {stampData[location].name}
+          </h1>
+          <Image
+            src={stampData[location].image}
+            alt=""
+            width={350}
+            height={350}
+          />
+          <p className="text-center text-slate-500 ">
+            {stampData[location].intro}
+          </p>
+          <StyledLink
+            className="w-full"
+            variant="contained"
+            text="Back to Stamp"
+            styleType="primary"
+            href={"/stamps"}
+          />
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default View;
