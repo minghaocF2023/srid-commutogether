c386d43e27fd42a3eb2fa8179b75a5837e87d6b5
Leo Chen <minghaoc@andrew.cmu.edu>
2024-12-03 23:52:49 -0800

    fix image style

---
+++ src/types/stamps.ts
@@ -0,0 +1,17 @@
+export type LocationData = {
+    name: string;
+    stampImage: string;
+    image: string;
+    intro: string;
+    collected: boolean;
+  };
+
+  export type SocialData = {
+    name: string;
+    stampImage: string;
+    image: string;
+    description: string;
+    timestamp: string;
+    collected: boolean;
+    count: number;
+  };
